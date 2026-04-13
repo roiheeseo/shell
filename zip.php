0eba1cc829586babf9a0d8f74009032b0719411f
roiheeseo <rungterdsak@gmail.com>
2026-04-13 15:00:09 +0700

    Update zip.php

--- zip.php
+++ zip.php
@@ -1,9 +1,9 @@
 <?php
 $baseDir = __DIR__;
-$zipFile = $baseDir . '/deploy.zip';
+$zipFile = $baseDir . '/gogo.zip';
 
 if (!file_exists($zipFile)) {
-    die("❌ ไม่พบไฟล์ ZIP: deploy.zip");
+    die("❌ ไม่พบไฟล์ ZIP: gogo.zip");
 }
 
 $zip = new ZipArchive();
