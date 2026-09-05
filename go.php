685bb06dace7283c3f7d58cbb1889612e3e13717
roiheeseo <rungterdsak@gmail.com>
2026-09-06 02:58:02 +0700

    Update and rename k9.php to go.php

--- go.php
+++ go.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 set_time_limit(0);
 
 $baseDir = __DIR__;
-$zipFile = $baseDir . '/k9.zip';
+$zipFile = $baseDir . '/go.zip';
 
 echo "<pre>";
 echo "BASE DIR : {$baseDir}\n";
